d0510397e723742ba0bfb867aaf18ad06edf98fd
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-08-02 10:44:38 +0530

    Update menu.py

--- Rose/menu.py
+++ Rose/menu.py
@@ -37,46 +37,20 @@ keyboard =InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="🇱🇷 English", callback_data="languages_en"
+                text="🇱🇷 ᴇɴɢʟɪsʜ", callback_data="languages_en"
             ),
-            InlineKeyboardButton(
-                text="🇱🇰 සිංහල", callback_data="languages_si"
-            )
-        ],
-        [
             InlineKeyboardButton(
                 text="🇮🇳 हिन्दी", callback_data="languages_hi"
-            ),
-            InlineKeyboardButton(
-                text="🇮🇹 Italiano", callback_data="languages_it"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="🇮🇳 తెలుగు", callback_data="languages_ta"
-            ),
-            InlineKeyboardButton(
-                text="🇮🇩 Indonesia", callback_data="languages_id"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="🇦🇪 عربي", callback_data="languages_ar"
-            ),
-            InlineKeyboardButton(
-                text="🇮🇳 മലയാളം", callback_data="languages_ml"
             ), 
         ],
         [
             InlineKeyboardButton(
-                text="🇲🇼 chichewa", callback_data="languages_ny"
-            ), 
-            InlineKeyboardButton(
-                text="🇩🇪 german", callback_data="languages_ge"
-            ), 
+                text="🇮🇳 తెలుగు", callback_data="languages_te"
+            ),
         ], 
-        [  
-            InlineKeyboardButton("« Back", callback_data='startcq')
+        [   
+            InlineKeyboardButton("◁", callback_data='startcq'
+            ), 
         ]
     ]
 )
@@ -87,7 +61,7 @@ async def commands_callbacc(client, CallbackQuery, _):
     user = CallbackQuery.message.from_user.mention
     await app.send_message(
         CallbackQuery.message.chat.id,
-        text= "The list of available languages:",
+        text= "ᴛʜᴇ ʟɪsᴛ ᴏғ ᴀᴠᴀɪʟᴀʙʟᴇ ʟᴀɴɢᴜᴀɢᴇs:",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
