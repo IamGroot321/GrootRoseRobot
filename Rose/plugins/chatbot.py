779b9d20aa5b5ec4cde501336a0dad8f365aafe8
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-08-02 10:53:20 +0530

    Update chatbot.py

--- Rose/plugins/chatbot.py
+++ Rose/plugins/chatbot.py
@@ -1,34 +1,29 @@
 import requests
 from pyrogram import filters
+from pyrogram.types import Message
 from googletrans import Translator
-from Rose import *
-from Rose.utils.filter_groups import *
+from Rose import app,BOT_ID
+from Rose.utils.filter_groups import cbot
 from lang import get_command
-from Rose.utils.lang import *
-from Rose.utils.commands import *
-from Rose import *
+from Rose.utils.lang import language
 from Rose.mongo import chatb
 from Rose.plugins.antlangs import get_arg
 from Rose.utils.custom_filters import admin_filter
-from button import *
+from button import Chat_Bot
+
 
 tr = Translator()
 CBOT = get_command("CBOT")
 CBOTA = get_command("CBOTA")
 
 
-@app.on_message(
-    filters.command("chatbot")
-    & ~filters.edited
-    & ~filters.private
-    & admin_filter
-)
+@app.on_message(filters.command("chatbot") & ~filters.edited& ~filters.private& admin_filter)
 @language
 async def cbots(client, message: Message, _):
     group_id = str(message.chat.id)
     chat_id = message.chat.id
     user_id = message.from_user.id
-    user = await bot.get_chat_member(group_id, user_id)
+    user = await app.get_chat_member(group_id, user_id)
     if not user.status == "creator" or user.status == "administrator":
         return
     if len(message.command) < 2:
@@ -39,22 +34,14 @@ async def cbots(client, message: Message, _):
     sex = await message.reply_text(_["antil2"])
     lower_args = args.lower()
     if lower_args == "on":
-        chatb.insert_one({f"chatbot": group_id})#default AI is Afflicate+
+        chatb.insert_one({f"chatbot": group_id})
     elif lower_args == "off":
         chatb.delete_one({f"chatbot": group_id})
     else:
         return await sex.edit(_["chatb1"])
     await sex.edit(f"✅ **Successfully** `{'Enabled' if lower_args=='on' else 'Disabled'}` ** Chat bot**")
 
-@app.on_message(
-    filters.text
-    & filters.reply
-    & ~filters.bot
-    & ~filters.via_bot
-    & ~filters.forwarded
-    & ~filters.private
-    & ~filters.edited,
-    group=cbot)
+@app.on_message(filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded & ~filters.private & ~filters.edited, group=cbot)
 async def szcbot(_, message: Message):
     chat_id = message.chat.id
     if not message.reply_to_message:
@@ -67,32 +54,42 @@ async def szcbot(_, message: Message):
         return
     chat = chatb.find_one({"chatbot":chat_id})   
     if chat:
-       await app.send_chat_action(message.chat.id, "typing")
-       lang = tr.translate(message.text).src
-       trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
-       text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-       affiliateplus = requests.get(f"https://api.safone.tech/chatbot?message={text}&bot_name=Bhanumathibot&bot_master=Mynameisgroot&user_id=5027740392")
-       textmsg = (affiliateplus.json()["message"])
-       if "Affiliate+" in textmsg:
-        textmsg = textmsg.replace("Affiliate+", "Bhanumathi bot created by @mynameisgroot")
-       if "Lebyy_Dev" in textmsg:
-        textmsg = textmsg.replace("Lebyy_Dev", "@MyNameIsGroot Owner of @BhanumathiBot")
-       if "God Brando" in textmsg:
-        textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
-       if "seeker" in textmsg:
-        textmsg = textmsg.replace("seeker", f"wow")
-       msg = tr.translate(textmsg, src='en', dest=lang)
-       await message.reply_text(msg.text)
+        await app.send_chat_action(message.chat.id, "typing")
+        try:
+           lang = tr.translate(message.text).src
+           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
+           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
+           affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Rose&ownername=@supunma&user={chat_id}")
+           textmsg = (affiliateplus.json()["message"])
+           if "Affiliate+" in textmsg:
+               textmsg = textmsg.replace("Affiliate+", "Rose")
+           if "Lebyy_Dev" in textmsg:
+               textmsg = textmsg.replace("Lebyy_Dev", "Supun Maduranga")
+           if "God Brando" in textmsg:
+               textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
+           if "seeker" in textmsg:
+               textmsg = textmsg.replace("seeker", f"wow")
+           msg = tr.translate(textmsg, src='en', dest=lang)
+           await message.reply_text(msg.text)
+        except Exception:
+           user_id = message.from_user.id
+           lang = tr.translate(message.text).src
+           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
+           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
+           safeone = requests.get(f"https://api.safone.tech/chatbot?message={text}&bot_name=Rose&bot_master=Supun&user_id={user_id}")
+           textmsg = (safeone.json()["answer"])
+           if "Affiliate+" in textmsg:
+               textmsg = textmsg.replace("Affiliate+", "Rose")
+           if "[Safone]" in textmsg:
+               textmsg = textmsg.replace("[Safone]", "Supun Maduranga")
+           msg = tr.translate(textmsg, src='en', dest=lang)
+           await message.reply_text(msg.text)
 
-
-
-
-
-__MODULE__ = f"{Chat_Bot}"
+__MODULE__ = Chat_Bot
 __HELP__ = """
 **Chatbot**
 
-AI based chatbot allows Bhanumathi to talk and provides a more interactive group chat experience.
+AI based chatbot allows rose to talk and provides a more interactive group chat experience.
 
 - /chatbot [ON/OFF]: Enables and disables Affiliate + AI Chat bot.
 
@@ -102,12 +99,6 @@ AI based chatbot allows Bhanumathi to talk and provides a more interactive group
 
 
 **Language Support**
-Bhanumathi AI chatbot support almost all languages in world .
+Rose AI chatbot support almost all languages in world .
 Powered By ; `googletrans==3.1.0a0`
 """
-
-
-
-
-
-
