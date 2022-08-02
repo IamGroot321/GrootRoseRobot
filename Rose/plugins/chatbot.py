c2e9cb8fdfe615dc2ac4f81d710a391213f45771
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-08-02 13:17:53 +0530

    Update chatbot.py

--- Rose/plugins/chatbot.py
+++ Rose/plugins/chatbot.py
@@ -59,12 +59,12 @@ async def szcbot(_, message: Message):
            lang = tr.translate(message.text).src
            trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
            text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-           affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Rose&ownername=@supunma&user={chat_id}")
+           affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=bhanumathi&ownername=@santhu_afk_bot7981&user={chat_id}")
            textmsg = (affiliateplus.json()["message"])
            if "Affiliate+" in textmsg:
-               textmsg = textmsg.replace("Affiliate+", "Rose")
+               textmsg = textmsg.replace("Affiliate+", "bhanumathi")
            if "Lebyy_Dev" in textmsg:
-               textmsg = textmsg.replace("Lebyy_Dev", "Supun Maduranga")
+               textmsg = textmsg.replace("Lebyy_Dev", "groot")
            if "God Brando" in textmsg:
                textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
            if "seeker" in textmsg:
@@ -76,12 +76,12 @@ async def szcbot(_, message: Message):
            lang = tr.translate(message.text).src
            trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
            text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-           safeone = requests.get(f"https://api.safone.tech/chatbot?message={text}&bot_name=Rose&bot_master=Supun&user_id={user_id}")
+           safeone = requests.get(f"https://api.safone.tech/chatbot?message={text}&user_id={user_id}&bot_name=BhanumathiBot&bot_master=Groot")
            textmsg = (safeone.json()["answer"])
            if "Affiliate+" in textmsg:
-               textmsg = textmsg.replace("Affiliate+", "Rose")
+               textmsg = textmsg.replace("Affiliate+", "bhanumathi")
            if "[Safone]" in textmsg:
-               textmsg = textmsg.replace("[Safone]", "Supun Maduranga")
+               textmsg = textmsg.replace("[Safone]", "groot")
            msg = tr.translate(textmsg, src='en', dest=lang)
            await message.reply_text(msg.text)
 
