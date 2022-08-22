77c0b76158ac098d1d6586aac67856dd9fe9f7b2
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-08-22 22:17:17 +0530

    Update Antiservice.py

--- Rose/plugins/Antiservice.py
+++ Rose/plugins/Antiservice.py
@@ -36,7 +36,7 @@ async def delete_service(_, message):
     chat_id = message.chat.id
     try:
         if await is_antiservice_on(chat_id):
-            return await message.delete()
+            return 
     except Exception:
         pass
     
